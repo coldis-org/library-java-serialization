853e154fe0ad9a011e8de54b29a795579d995bb9
rvcoutinho <me@rvcoutinho.com>
2024-08-14 00:19:52 -0300

    - Improving serialization class selction;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/serialization/OptimizedSerializationHelper.java
+++ src/main/java/org/coldis/library/serialization/OptimizedSerializationHelper.java
@@ -1,8 +1,10 @@
 package org.coldis.library.serialization;
 
-import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Inherited;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.fury.BaseFury;
@@ -10,14 +12,101 @@ import org.apache.fury.Fury;
 import org.apache.fury.config.CompatibleMode;
 import org.apache.fury.config.FuryBuilder;
 import org.apache.fury.config.Language;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.core.type.classreading.MetadataReader;
-import org.springframework.core.type.classreading.MetadataReaderFactory;
-import org.springframework.core.type.filter.TypeFilter;
+import org.springframework.core.type.filter.AbstractTypeHierarchyTraversingFilter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ClassUtils;
 
 /**
  * Optimized serialization helper.
  */
 public class OptimizedSerializationHelper {
+
+	/**
+	 * No annotation type filter.
+	 */
+	static class NoAnnotationTypeFilter extends AbstractTypeHierarchyTraversingFilter {
+
+		/** Without annotations. */
+		private final Set<Class<? extends Annotation>> withoutAnnotations;
+
+		/** Consider meta-annotations. */
+		private final boolean considerMetaAnnotations;
+
+		/**
+		 * Constructor.
+		 *
+		 * @param annotations             Annotations.
+		 * @param considerMetaAnnotations Consider meta-annotations.
+		 * @param considerInterfaces      Consider interfaces.
+		 */
+		public NoAnnotationTypeFilter(
+				final Set<Class<? extends Annotation>> withoutAnnotations,
+				final boolean considerMetaAnnotations,
+				final boolean considerInterfaces) {
+			super(withoutAnnotations.stream().anyMatch(annotationType -> annotationType.isAnnotationPresent(Inherited.class)), considerInterfaces);
+			this.withoutAnnotations = withoutAnnotations;
+			this.considerMetaAnnotations = considerMetaAnnotations;
+		}
+
+		/**
+		 * Checks if the class has any annotation.
+		 *
+		 * @param  typeName Type name.
+		 * @return          If the class has any annotation.
+		 */
+		@Nullable
+		protected Boolean hasAnyAnnotation(
+				final String typeName) {
+			Boolean hasAnnotations = false;
+			try {
+				final Class<?> clazz = ClassUtils.forName(typeName, this.getClass().getClassLoader());
+				hasAnnotations = this.withoutAnnotations.stream()
+						.anyMatch(annotationType -> ((this.considerMetaAnnotations ? AnnotationUtils.getAnnotation(clazz, annotationType)
+								: clazz.getAnnotation(annotationType)) != null));
+			}
+			catch (final Throwable throwable) { // Ignores.
+			}
+			return hasAnnotations;
+		}
+
+		/**
+		 * @see org.springframework.core.type.filter.AnnotationTypeFilter#matchSelf(org.springframework.core.type.classreading.MetadataReader)
+		 */
+		@Override
+		protected boolean matchSelf(
+				final MetadataReader metadataReader) {
+			final AnnotationMetadata metadata = metadataReader.getAnnotationMetadata();
+			return this.withoutAnnotations.stream().noneMatch(annotationType -> metadata.hasAnnotation(annotationType.getName())
+					|| (this.considerMetaAnnotations && metadata.hasMetaAnnotation(annotationType.getName())));
+		}
+
+		/**
+		 * @see org.springframework.core.type.filter.AnnotationTypeFilter#matchSuperClass(java.lang.String)
+		 */
+		@Override
+		protected Boolean matchSuperClass(
+				final String superClassName) {
+			return !this.hasAnyAnnotation(superClassName);
+		}
+
+		/**
+		 * @see org.springframework.core.type.filter.AnnotationTypeFilter#matchInterface(java.lang.String)
+		 */
+		@Override
+		protected Boolean matchInterface(
+				final String interfaceName) {
+			return !this.hasAnyAnnotation(interfaceName);
+		}
+
+	}
 
 	/**
 	 * Creates a serializer.
@@ -37,15 +126,11 @@ public class OptimizedSerializationHelper {
 				: furyBuilder.buildThreadSafeFury()) : furyBuilder.build());
 		if (ArrayUtils.isNotEmpty(packagesNames)) {
 			final Map<Class<?>, String> modelClasses = new HashMap<>();
-			modelClasses.putAll(ObjectMapperHelper.getModelClasses(new TypeFilter() {
-
-				@Override
-				public boolean match(
-						final MetadataReader metadataReader,
-						final MetadataReaderFactory metadataReaderFactory) throws IOException {
-					return true;
-				}
-			}, packagesNames));
+			modelClasses
+					.putAll(ObjectMapperHelper.getModelClasses(
+							new OptimizedSerializationHelper.NoAnnotationTypeFilter(
+									Set.of(Component.class, Controller.class, Service.class, Repository.class, Configuration.class), true, true),
+							packagesNames));
 			modelClasses.forEach((
 					clazz,
 					className) -> {
--- src/test/java/org/coldis/library/test/serialization/OptimizedSerializationHelperTest.java
+++ src/test/java/org/coldis/library/test/serialization/OptimizedSerializationHelperTest.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 import org.apache.fury.Fury;
 import org.apache.fury.config.Language;
-import org.apache.fury.resolver.MetaContext;
 import org.coldis.library.serialization.OptimizedSerializationHelper;
 import org.coldis.library.test.serialization.dto.DtoTestObject2Dto;
 import org.coldis.library.test.serialization.dto.DtoTestObjectDto;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -37,10 +35,12 @@ public class OptimizedSerializationHelperTest {
 	private static Fury serializer2;
 
 	static {
-		OptimizedSerializationHelperTest.serializer1 = (Fury) OptimizedSerializationHelper.createSerializer(false, null, null, Language.XLANG,
-				"org.coldis.library.test.serializationX");
-		OptimizedSerializationHelperTest.serializer1.register(DtoTestObjectDto.class, DtoTestObject.class.getName());
-		OptimizedSerializationHelperTest.serializer1.register(DtoTestObject2Dto.class, DtoTestObject2.class.getName());
+		OptimizedSerializationHelperTest.serializer1 = (Fury) OptimizedSerializationHelper.createSerializer(false, null, null, Language.JAVA,
+		"org.coldis.library.test.serialization");
+//		OptimizedSerializationHelperTest.serializer1 = (Fury) OptimizedSerializationHelper.createSerializer(false, null, null, Language.XLANG,
+//		"org.coldis.library.test.serializationX");
+//		OptimizedSerializationHelperTest.serializer1.register(DtoTestObjectDto.class, DtoTestObject.class.getName());
+//		OptimizedSerializationHelperTest.serializer1.register(DtoTestObject2Dto.class, DtoTestObject2.class.getName());
 		OptimizedSerializationHelperTest.serializer2 = (Fury) OptimizedSerializationHelper.createSerializer(false, null, null, Language.XLANG,
 				"org.coldis.library.test.serializationX");
 		OptimizedSerializationHelperTest.serializer2.register(DtoTestObject.class, DtoTestObject.class.getName());
@@ -53,15 +53,15 @@ public class OptimizedSerializationHelperTest {
 	 * @throws Exception If the test does not succeed.
 	 */
 	@Test
-	@Disabled
+	// @Disabled
 	public void test01Serialization() throws Exception {
 		// For each test data.
 		for (final DtoTestObjectDto originalDto : OptimizedSerializationHelperTest.TEST_DATA) {
 			// Converts the DTO to the original object and back.
 			long start = System.currentTimeMillis();
 			final byte[] serializedObject1 = OptimizedSerializationHelperTest.serializer1.serialize(originalDto);
-			final DtoTestObject originalObject1 = (DtoTestObject) OptimizedSerializationHelperTest.serializer2.deserialize(serializedObject1);
-			final byte[] reserializedObject1 = OptimizedSerializationHelperTest.serializer2.serialize(originalObject1);
+			final DtoTestObjectDto originalObject1 = (DtoTestObjectDto) OptimizedSerializationHelperTest.serializer1.deserialize(serializedObject1);
+			final byte[] reserializedObject1 = OptimizedSerializationHelperTest.serializer1.serialize(originalObject1);
 			final DtoTestObjectDto reconvertedDto1 = (DtoTestObjectDto) OptimizedSerializationHelperTest.serializer1.deserialize(reserializedObject1);
 			System.out.println("Serialization and deserialization took " + (System.currentTimeMillis() - start) + "ms.");
 
